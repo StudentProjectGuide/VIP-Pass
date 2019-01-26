77b78294a6bbf686f1fcc0d89dc46bb2527285f4
Student Project Guide <33988225+StudentProjectGuide@users.noreply.github.com>
2019-01-27 00:01:11 +0530

    Initial commit

---
+++ db_login.php
@@ -0,0 +1,19 @@
+<?php
+//Modified from studentprojectguide.com
+	$db_host='localhost';
+	$db_username='root';
+	$db_password='';
+	$db_name="book";
+	$connection = mysqli_connect($db_host, $db_username, $db_password,$db_name);
+	if (!$connection)
+	{
+		die ("Could not connect to the database: <br />". mysql_error());
+	}
+?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-2373433789016050",
+          enable_page_level_ads: true
+     });
+</script>
